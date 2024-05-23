11d6f7ff6f22bc4ff3123475ec6bde1384c6c24e
TranHBach <ragoza0932447933@gmail.com>
2024-05-23 15:56:05 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react"
+import { IndexeddbPersistence } from "y-indexeddb"
+import { WebrtcProvider } from "y-webrtc"
+import * as Y from "yjs"
+import { YjsTextarea } from "./YjsTextArea"
+
+const room = "pengx17-test-textarea"
+
+const usercolors = [
+  "#30bced",
+  "#6eeb83",
+  "#ffbc42",
+  "#ecd444",
+  "#ee6352",
+  "#9ac2c9",
+  "#8acb88",
+  "#1be7ff"
+]
+const myColor = usercolors[Math.floor(Math.random() * usercolors.length)]
 
 function App() {
+  const [yText, setYText] = useState()
+  const [awareness, setAwareness] = useState()
+
+  useEffect(() => {
+    const yDoc = new Y.Doc()
+    const persistence = new IndexeddbPersistence(room, yDoc)
+    const wrtcProvider = new WebrtcProvider(room, yDoc)
+
+    wrtcProvider.awareness.setLocalStateField("user", {
+      color: myColor
+    })
+
+    persistence.once("synced", () => {
+      console.log("synced")
+      const yText = yDoc.getText("text")
+      setYText(yText)
+      setAwareness(wrtcProvider.awareness)
+    })
+
+    return () => {
+      yDoc.destroy()
+      persistence.destroy()
+      wrtcProvider.destroy()
+      setYText(undefined)
+      setAwareness(undefined)
+    }
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <YjsTextarea yText={yText} awareness={awareness} />
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
