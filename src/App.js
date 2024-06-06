4671efb1a51efbe8dee5705eb965bf7b61703db5
QuocAnVuong <vuongquocan2308@gmail.com>
2024-06-06 09:15:04 +0700

    Restore to Demo

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 import TextPage from "./Page/TextPage";
+import Landing from "./Page/Landing";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
 //import Landing from "./Page/Landing";
 import ContextProvider from "./Context/ContextProvider";
 
 const router = createBrowserRouter([
-  /*{
+  {
     path: "/",
     element: <Landing />,
     errorElement: <h1>404 Not Found</h1>,
-  },*/
+  },
   {
-    path: "/",
+    path: "/text",
     element: <TextPage />,
   },
 ]);
