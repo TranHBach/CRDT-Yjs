318038b94c6d3092eeab950359e816e9c02ee440
QuocAnVuong <vuongquocan2308@gmail.com>
2024-06-09 17:52:27 +0700

    Restore Default

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
 import TextPage from "./Page/TextPage";
 import "./App.css";
 import { RouterProvider, createBrowserRouter } from "react-router-dom";
-//import Landing from "./Page/Landing";
+import Landing from "./Page/Landing";
 import ContextProvider from "./Context/ContextProvider";
 
 const router = createBrowserRouter([
-  /*{
+  {
     path: "/",
     element: <Landing />,
     errorElement: <h1>404 Not Found</h1>,
-  },*/
+  },
   {
-    path: "/",
+    path: "/text",
     element: <TextPage />,
   },
 ]);
--- src/Page/TextPage.js
+++ src/Page/TextPage.js
@@ -4,7 +4,7 @@ import { IndexeddbPersistence } from "y-indexeddb";
 import { WebrtcProvider } from "y-webrtc";
 import { YjsTextarea } from "../YjsTextArea";
 import History from "../Components/History";
-import { PasswordContext } from "../Context/ContextProvider";
+import { PasswordContext, RoomContext } from "../Context/ContextProvider";
 import { openDB } from "idb";
 import { ADJECTIVES, ANIMALS } from "../Name/cursorNames";
 import { COLOR } from "../Name/cssColors";
@@ -21,8 +21,8 @@ function capitalizeFirstLetter(string) {
 }
 
 function TextPage() {
-  //const { room } = useContext(RoomContext);
-  const room = "1";
+  const { room } = useContext(RoomContext);
+  //const room = "1";
   const { password } = useContext(PasswordContext);
   const [yText, setYText] = useState();
   const [awareness, setAwareness] = useState();
