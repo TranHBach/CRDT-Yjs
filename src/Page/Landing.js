d7c312b68386b673d563acd5ce84d46a1a25fae4
QuocAnVuong <vuongquocan2308@gmail.com>
2024-06-12 13:17:19 +0700

    Fix Landing responsive

--- src/Page/Landing.js
+++ src/Page/Landing.js
@@ -37,8 +37,13 @@ function Landing() {
   return (
     <div className="h-screen flex justify-center items-center roboto-regular">
       <div>
-        <p className="text-[44px]">Colaborative Editor</p>
-        <div className="flex justify-between">
+        <p className="text-center text-[22px] lg:text-[44px]">
+          Colaborative Editor
+        </p>
+        <div
+          className="flex flex-col items-center justify-center gap-y-3
+        lg:justify-between lg:gap-y-0 lg:flex-row"
+        >
           <div
             className="flex justify-between items-center bg-[#1A73E8] text-white h-[40px] cursor-pointer select-none
           px-[10px] py-[5px] rounded-[10px] mr-[20px]"
